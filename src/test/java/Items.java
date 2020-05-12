e5baf053983a83de7bc07558da60a231816f1e28
rusakov66 <rusakov66@gmail.com>
2020-05-12 17:10:30 +0300

    Class Media was added

--- src/test/java/Items.java
+++ src/test/java/Items.java
@@ -2,7 +2,7 @@ public class Items {
 
         private String title;
         private String link;
-        private String media;
+        private Media media;
         private String date_taken;
         private String description;
         private String published;
@@ -12,7 +12,8 @@ public class Items {
 
     public Items() {
     }
-    public Items(String title, String link, String media, String date_taken, String description, String published, String author, String author_id, String tags) {
+
+    public Items(String title, String link, Media media, String date_taken, String description, String published, String author, String author_id, String tags) {
         this.title = title;
         this.link = link;
         this.media = media;
@@ -21,42 +22,6 @@ public class Items {
         this.published = published;
         this.author = author;
         this.author_id = author_id;
-        this.tags = tags;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public void setMedia(String media) {
-        this.media = media;
-    }
-
-    public void setDate_taken(String date_taken) {
-        this.date_taken = date_taken;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPublished(String published) {
-        this.published = published;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setAuthor_id(String author_id) {
-        this.author_id = author_id;
-    }
-
-    public void setTags(String tags) {
         this.tags = tags;
     }
 
@@ -64,36 +29,72 @@ public class Items {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getLink() {
         return link;
     }
 
-    public String getMedia() {
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Media getMedia() {
         return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
     }
 
     public String getDate_taken() {
         return date_taken;
     }
 
+    public void setDate_taken(String date_taken) {
+        this.date_taken = date_taken;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getPublished() {
         return published;
     }
 
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getAuthor_id() {
         return author_id;
     }
 
+    public void setAuthor_id(String author_id) {
+        this.author_id = author_id;
+    }
+
     public String getTags() {
         return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
     }
 
     @Override
@@ -101,7 +102,7 @@ public class Items {
         return "Items{" +
                 "title='" + title + '\'' +
                 ", link='" + link + '\'' +
-                ", media='" + media + '\'' +
+                ", media=" + media +
                 ", date_taken='" + date_taken + '\'' +
                 ", description='" + description + '\'' +
                 ", published='" + published + '\'' +
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,36 +1,18 @@
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.restassured.response.Response;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONString;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Set;
-
-import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
-import static com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_SINGLE_QUOTES;
-import static com.fasterxml.jackson.databind.DeserializationFeature.*;
-import static com.fasterxml.jackson.databind.SerializationFeature.FAIL_ON_EMPTY_BEANS;
-import static io.restassured.RestAssured.get;
-import java.net.URL;
-// import static io.restassured.RestAssured.get;
-
-
-
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws JSONException, IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Flickr flickr = objectMapper.readValue(new URL("https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"), Flickr.class);
-        Items items = new Items();
+        //Items items = new Items();
 
+        System.out.println(flickr.toString());
         System.out.println(flickr.items.toString());
 
 

d2b2644f1cca53610267cbf99385c72f520e197b
rusakov66 <rusakov66@gmail.com>
2020-05-12 14:51:58 +0300

    removed conflict message

--- src/test/java/Flickr.java
+++ src/test/java/Flickr.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-<<<<<<< Updated upstream
 
 public class Flickr {
     public String title;
@@ -8,85 +7,16 @@ public class Flickr {
     public String modified;
     public String generator;
     ArrayList< Object > items = new ArrayList < Object > ();
-
-
-
-    // Getter Methods
-=======
-import java.util.List;
-
-public class Flickr {
-    private String title;
-    private String link;
-    private String description;
-    private String modified;
-    private String generator;
-    private List<Items> items;
-
     public Flickr() {
+
     }
 
-    public Flickr(String title, String link, String description, String modified, String generator, List<Items> items) {
+    public Flickr(String title, String link, String description, String modified, String generator, ArrayList<Object> items) {
         this.title = title;
         this.link = link;
         this.description = description;
         this.modified = modified;
         this.generator = generator;
-        this.items = items;
-    }
->>>>>>> Stashed changes
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getModified() {
-        return modified;
-    }
-
-    public String getGenerator() {
-        return generator;
-    }
-
-<<<<<<< Updated upstream
-    // Setter Methods
-=======
-    public List<Items> getItems() {
-        return items;
-    }
->>>>>>> Stashed changes
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setModified(String modified) {
-        this.modified = modified;
-    }
-
-    public void setGenerator(String generator) {
-        this.generator = generator;
-    }
-<<<<<<< Updated upstream
-=======
-
-    public void setItems(List<Items> items) {
         this.items = items;
     }
 
@@ -101,5 +31,52 @@ public class Flickr {
                 ", items=" + items +
                 '}';
     }
->>>>>>> Stashed changes
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public String getGenerator() {
+        return generator;
+    }
+
+    public void setGenerator(String generator) {
+        this.generator = generator;
+    }
+
+    public ArrayList<Object> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Object> items) {
+        this.items = items;
+    }
 }
